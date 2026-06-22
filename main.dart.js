a10bc1cfacb92c2f0cb404f1372cf1dd4089bcad
rdagmr98 <rdagmr98@users.noreply.github.com>
2026-06-22 23:15:57 +0000

    deploy: 6638e1605fd78d90ecd52f6063f72f1fee4fd479

--- main.dart.js
+++ main.dart.js
@@ -30650,7 +30650,7 @@ $S:2}
 A.aFG.prototype={
 $1(a){var s=A.dK().b
 s=s==null?null:s.canvasKitBaseUrl
-return(s==null?"https://www.gstatic.com/flutter-canvaskit/77e2e94772b6eb43759e34ed1ad7da4674e19cab/":s)+a},
+return(s==null?"https://www.gstatic.com/flutter-canvaskit/a4ce257c68517c1410f4b48ac9852ab5642a3f8d/":s)+a},
 $S:72}
 A.PH.prototype={
 gB(a){var s=this.a
